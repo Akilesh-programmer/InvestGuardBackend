6d1e4f9c23873b1556eacf00c9b14802d7e37f06
EnochJasonJ <enochjason06@gmail.com>
2025-03-31 12:03:47 +0530

    Added venv installation to build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
-
+python3 -m venv venv
+source ./venv/bin/activate
+pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 gunicorn project.wsgi:application --bind 0.0.0.0:8000
